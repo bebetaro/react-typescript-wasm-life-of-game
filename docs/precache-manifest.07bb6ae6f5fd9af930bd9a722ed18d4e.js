e885960e1e96d086d9616b9da5f9fb62f0020c44
bebetaro <kurokawa@aptpod.co.jp>
2020-07-10 12:36:22 +0900

    fix package.json and rebuild

---
+++ docs/precache-manifest.07bb6ae6f5fd9af930bd9a722ed18d4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769233c8fc50cd52c62f",
+    "url": "./be21e3da6a6b488860d2.module.wasm"
+  },
+  {
+    "revision": "f4054403db8fb8d53c89640cdd00fca1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec54f7f00d30ca37f2a9",
+    "url": "./static/css/main.c0cd5df2.chunk.css"
+  },
+  {
+    "revision": "769233c8fc50cd52c62f",
+    "url": "./static/js/0.038c6370.chunk.js"
+  },
+  {
+    "revision": "3211dce4954cf2dc7cb1",
+    "url": "./static/js/3.c839a087.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/3.c839a087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769fcd513aa3aa12f7d4",
+    "url": "./static/js/4.b3bcb426.chunk.js"
+  },
+  {
+    "revision": "ec54f7f00d30ca37f2a9",
+    "url": "./static/js/main.ea8b2671.chunk.js"
+  },
+  {
+    "revision": "9953c5eccac10d2f38df",
+    "url": "./static/js/runtime-main.0096dfcb.js"
+  }
+]);
